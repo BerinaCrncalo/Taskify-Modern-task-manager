848af8493b98740229b35c0fd1034ecb5c0979d9
BerinaCrncalo <berina.crncalo@stu.ibu.edu.ba>
2024-06-12 19:12:10 +0200

    Loader added

--- app/src/main/java/com/example/taskify_moderntaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskify_moderntaskmanager/MainActivity.kt
@@ -7,8 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import com.example.taskify_moderntaskmanager.ui.theme.TaskifyModernTaskManagerTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +24,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskifyModernTaskManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Modifier.padding(innerPadding).TaskifyApp()
+                var showLoader by remember { mutableStateOf(true) }
+                LaunchedEffect(Unit) {
+                    lifecycleScope.launch {
+                        delay(3000) //Showing loader for 3 seconds
+                        showLoader = false
+                    }
+                }
+                if (showLoader) {
+                    Loader()
+                } else {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Modifier.padding(innerPadding).TaskifyApp()
+                    }
                 }
             }
         }
